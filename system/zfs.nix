f28613373b0639af5146e41146c91beb0b20c6e8
gsegt <git@gsegt.eu>
2025-06-16 16:54:03 +0200

    feat: add zfs load keys service to unlock encrypted datasets automatically

--- system/zfs.nix
+++ system/zfs.nix
@@ -6,4 +6,17 @@
   networking.hostId = "228b6aee";
 
   services.zfs.autoScrub.enable = true;
+
+  systemd.services.zfs-load-keys = {
+    description = "Load zfs encryption keys";
+    wantedBy = [ "zfs-mount.service" ];
+    before = [ "zfs-mount.service" ];
+    after = [ "zfs-import.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = [ "/run/current-system/sw/bin/zfs load-key -a" ];
+      StandardInput = "tty-force";
+    };
+  };
 }
