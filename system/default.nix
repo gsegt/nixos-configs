1d953efadac6e2c8cc3673306c017eb74481b712
gsegt <git@gsegt.eu>
2025-06-16 11:06:47 +0200

    feat: optimise swap related systemctl settings

--- system/default.nix
+++ system/default.nix
@@ -14,6 +14,12 @@
       };
     };
     initrd.checkJournalingFS = false;
+    kernel.sysctl = {
+      "vm.swappiness" = 180;
+      "vm.watermark_boost_factor" = 0;
+      "vm.watermark_scale_factor" = 125;
+      "vm.page-cluster" = 0;
+    };
   };
 
   networking.hostName = "nixos";
