e355aebb9f02d8f65daed3768b794eacc093c6dd
gsegt <git@gsegt.eu>
2025-06-16 18:55:30 +0200

    feat: optimise swap related systemctl settings

--- system/zram.nix
+++ system/zram.nix
@@ -1,4 +1,11 @@
 {
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 180;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+    "vm.page-cluster" = 0;
+  };
+
   zramSwap = {
     enable = true;
     memoryPercent = 100;
