e4685ee8779166adb60f7b66e9f5ba010b1de2d9
gsegt <git@gsegt.eu>
2025-08-13 10:13:43 +0200

    refactor: make system/feature/dns a proper module

--- system/features/dns/default.nix
+++ system/features/dns/default.nix
@@ -1,6 +1,21 @@
 {
-  networking.nameservers = [
-    "1.1.1.1"
-    "1.0.0.1"
-  ];
+  lib,
+  config,
+  ...
+}:
+
+let
+  cfg = config.dns;
+in
+{
+  options.dns = {
+    enable = lib.mkEnableOption "Enable custom dns settings.";
+  };
+
+  config = lib.mkIf cfg.enable {
+    networking.nameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
+  };
 }
