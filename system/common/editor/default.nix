4120f5734e5054fd7a94f70b77874248c983577a
gsegt <git@gsegt.eu>
2025-08-13 17:43:37 +0200

    refactor: make system/common/editor a proper sub module of common

--- system/common/editor/default.nix
+++ system/common/editor/default.nix
@@ -1,8 +1,23 @@
 {
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
+  lib,
+  config,
+  ...
+}:
+
+let
+  cfg = config.common.editor;
+in
+{
+  options.common.editor = {
+    enable = lib.mkEnableOption "Enable common editor settings for all systems";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+    };
   };
 }
