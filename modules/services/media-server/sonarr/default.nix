7a80501eb3b858ad0cd290db0110dfcfb7b39c5e
gsegt <git@gsegt.eu>
2025-08-29 17:44:23 +0200

    feat(media-server): add support for sonarr

---
+++ modules/services/media-server/sonarr/default.nix
@@ -0,0 +1,20 @@
+{ lib, config, ... }:
+
+let
+  service = "sonarr";
+  cfg = config.modules.services.media-server.${service};
+in
+{
+  options.modules.services.media-server.${service} = {
+    enable = lib.mkEnableOption "Whether to enable custom ${service} settings.";
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.${service} = {
+      enable = true;
+      openFirewall = true;
+      user = config.modules.base.userName;
+      group = config.modules.base.userName;
+    };
+  };
+}
