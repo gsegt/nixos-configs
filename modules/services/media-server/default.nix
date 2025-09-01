5c027233ba8b5c9b3023210b05a657c3262dd26a
gsegt <git@gsegt.eu>
2025-09-01 18:10:11 +0200

    feat: add new service.media-server module

---
+++ modules/services/media-server/default.nix
@@ -0,0 +1,18 @@
+{ lib, config, ... }:
+
+let
+  utils = import ../../../utils;
+  cfg = config.modules.services.media-server;
+in
+{
+  imports = utils.importSubmodules { dir = ./.; };
+
+  options.modules.services.media-server = {
+    enable = lib.mkEnableOption "Whether to enable custom media server settings.";
+  };
+
+  config = lib.mkIf cfg.enable {
+    modules.services.media-server = {
+    };
+  };
+}
