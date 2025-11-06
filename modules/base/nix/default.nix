37d1e49ae638010dbe98a47fe6699f06710a2e50
gsegt <git@gsegt.eu>
2025-11-06 18:19:16 +0100

    feat: change garbage collection to run more aggressively and daily on default time

--- modules/base/nix/default.nix
+++ modules/base/nix/default.nix
@@ -16,8 +16,7 @@ in
       ];
       gc = {
         automatic = true;
-        dates = "weekly";
-        options = "--delete-older-than 30d";
+        options = "--delete-older-than 7d";
       };
       optimise.automatic = true;
     };
