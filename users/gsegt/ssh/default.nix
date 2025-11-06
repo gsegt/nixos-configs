407b23d1156643b555f405ba915293c65d01620c
gsegt <git@gsegt.eu>
2025-11-06 18:30:39 +0100

    refactor(ssh): use matchBlocks instead extraConfig

--- users/gsegt/ssh/default.nix
+++ users/gsegt/ssh/default.nix
@@ -1,11 +1,11 @@
 {
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      #: github.com - commit/pull/push to github.com
-      Host github.com gist.github.com
-          HostName github.com
-          IdentityFile ~/.ssh/github
-    '';
+    matchBlocks = {
+      "github.com gist.github.com" = {
+        hostname = "github.com";
+        identityFile = "~/.ssh/github";
+      };
+    };
   };
 }
