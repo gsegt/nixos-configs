23a3aa11be755661d7907fcf2f124981bb86c6c8
gsegt <git@gsegt.eu>
2026-03-15 14:42:36 +0100

    feat: add ygege custom config to prowlarr

--- modules/services/media-server/prowlarr/default.nix
+++ modules/services/media-server/prowlarr/default.nix
@@ -1,4 +1,9 @@
-{ lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 let
   service = "prowlarr";
@@ -13,6 +18,20 @@ in
     services.${service} = {
       enable = true;
       openFirewall = true;
+    };
+
+    systemd.tmpfiles.rules = [
+      "d ${config.services.prowlarr.dataDir}/Definitions/Custom 0755 nobody nogroup -"
+    ];
+
+    systemd.services.prowlarr-file-deploy = {
+      description = "Deploy ygege.yml for Prowlarr";
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig.Type = "oneshot";
+      script = ''
+        mkdir -p ${config.services.prowlarr.dataDir}/Definitions/Custom
+        ${pkgs.coreutils}/bin/install -m 644 -o nobody -g nogroup ${./ygege.yml} ${config.services.prowlarr.dataDir}/Definitions/Custom/ygege.yml
+      '';
     };
   };
 }
