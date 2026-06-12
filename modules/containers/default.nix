2cd1d8f0218482f208d24e30762ca2372f6663da
gsegt <git@gsegt.eu>
2026-06-12 20:54:02 +0200

    feat(containers): update definition according to latest compose2nix

--- modules/containers/default.nix
+++ modules/containers/default.nix
@@ -10,23 +10,19 @@ in
 
   config = lib.mkIf cfg.enable {
     # Runtime
-    virtualisation.oci-containers.backend = "podman";
-
     virtualisation.podman = {
       enable = true;
       autoPrune.enable = true;
       dockerCompat = true;
-      defaultNetwork.settings = {
-        # Required for container networking to be able to use names.
-        dns_enabled = true;
-      };
     };
 
-    # Enable container name DNS for non-default Podman networks.
-    # https://github.com/NixOS/nixpkgs/issues/226365
-    networking.firewall.interfaces."podman+".allowedUDPPorts = [ 53 ];
-
-    # Used instead of users.users.<myuser>.extraGroups = [ "podman" ]; to maintain modularity
-    users.extraGroups.podman.members = [ config.modules.base.userName ];
+    # Enable container name DNS for all Podman networks.
+    networking.firewall.interfaces =
+      let
+        matchAll = if !config.networking.nftables.enable then "podman+" else "podman*";
+      in
+      {
+        "${matchAll}".allowedUDPPorts = [ 53 ];
+      };
   };
 }
